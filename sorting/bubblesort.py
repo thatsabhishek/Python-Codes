36f08c6b180e04a3e956ae3df3cb0984b8bf4249
Abhishek Gupta <83904358+Abhishek-gupta-1911@users.noreply.github.com>
2023-03-16 18:40:52 +0530

    added bubble sort program

---
+++ sorting/bubblesort.py
@@ -0,0 +1,24 @@
+
+def bubblesort(arr, n):
+    for i in range(n):
+        for j in range(n-i-1):
+            if arr[j]>arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+n = int(input("Enter the number of elements: "))
+arr = []
+for i in range(n):
+    a = int(input("Enter element: "))
+    arr.append(a)
+print()
+
+print("Unsorted Array: ", end='')
+for i in range(n):
+    print(arr[i], end=' ')
+print()
+    
+bubblesort(arr, n)
+
+print("Sorted Array: ", end='')
+for i in range(n):
+    print(arr[i], end=' ')
